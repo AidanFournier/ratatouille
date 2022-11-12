39265fce88fea22480f378346f4bb8b353c22925
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-11-12 21:57:45 +0900

    coords state updating with lat and long

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -7,37 +7,49 @@ import axios from 'axios';
 import { Form } from './Form';
 import { HomeHeadline, HomeWrapper } from './styles';
 
-
-type Coords = {
-  lat: string,
-  long: string
-}
+const defaultCoords = {
+  lat: "",
+  long: ""
+};
+// type Coords = {
+//   lat: string,
+//   long: string
+// };
 
 export function Home(): JSX.Element {
   const [t, { language }] = useTranslation();
 
   const [ location, setLocation ] = useState("");
-  const [ coords, setCoords ] = useState([]);
+  const [ coords, setCoords ] = useState(defaultCoords);
+  const { lat, long } = coords;
 
 
   const urlCoords = `https://staging-snap.tablecheck.com/v2/autocomplete?locale=en&shop_universe_id=57e0b91744aea12988000001&text=${location}`;
-  const urlShops = `https://staging-snap.tablecheck.com/v2/shop_search?cuisines[]=kaiseki&geo_latitude=${coords[0]}&geo_longitude=${coords[1]}&shop_universe_id=57e0b91744aea12988000001&locale=en&per_page=50`
+  const urlShops = `https://staging-snap.tablecheck.com/v2/shop_search?cuisines[]=kaiseki&geo_latitude=${coords.lat}&geo_longitude=${coords.long}&shop_universe_id=57e0b91744aea12988000001&locale=en&per_page=50`
 
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     try { 
       console.log(urlCoords)
       const res = await axios.get(urlCoords);
-      console.log(res);
-      // setCoords(response.)
+      console.log(res.data);
+      const resLat: string = res.data.locations[0].payload.geo.lat.toString();
+      const resLong: string = res.data.locations[0].payload.geo.lon.toString();
+      const newCoords = { 
+        lat: resLat,
+        long: resLong
+      }
+      setCoords(newCoords)
+      // console.log(coords);
     } catch (err) {
-      console.log(err)
+      console.log('👹 ERROR:' + err)
     };
   };
 
+  console.log(coords);
+
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setLocation((e.target.value));
-    
   };
 
   return (
