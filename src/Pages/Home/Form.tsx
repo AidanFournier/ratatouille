7d27f5e5c907d8e94dc81d50e34a3677812b4d8e
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-11-12 19:14:06 +0900

    Autocomplete endpoint returning data

---
+++ src/Pages/Home/Form.tsx
@@ -0,0 +1,22 @@
+type FormProps = {
+    value: string[]
+}
+
+export function Form(): JSX.Element {
+    return (
+        <div className="form">
+            {/* <input 
+                type="text"
+                placeholder="Villager's name"
+                className="form--input"
+                name="villager"
+                value={props.value}
+                onChange={props.handleChange}
+            />
+            <button 
+                className="form--button"
+                onClick={props.handleClick}> */}
+            {/* </button> */}
+        </div>
+  )
+}
--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,15 +1,59 @@
 import { Helmet } from 'react-helmet';
 import { useTranslation } from 'react-i18next';
 import { Outlet } from 'react-router-dom';
+import { useState } from 'react'
+import axios from 'axios';
 
+import { Form } from './Form';
 import { HomeHeadline, HomeWrapper } from './styles';
+
+
+type Coords = {
+  lat: string,
+  long: string
+}
 
 export function Home(): JSX.Element {
   const [t, { language }] = useTranslation();
 
+  const [ location, setLocation ] = useState("");
+  const [ coords, setCoords ] = useState([]);
+
+
+  const urlCoords = `https://staging-snap.tablecheck.com/v2/autocomplete?locale=en&shop_universe_id=57e0b91744aea12988000001&text=${location}`;
+  const urlShops = `https://staging-snap.tablecheck.com/v2/shop_search?cuisines[]=kaiseki&geo_latitude=${coords[0]}&geo_longitude=${coords[1]}&shop_universe_id=57e0b91744aea12988000001&locale=en&per_page=50`
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try { 
+      console.log(urlCoords)
+      const res = await axios.get(urlCoords);
+      console.log(res);
+      // setCoords(response.)
+    } catch (err) {
+      console.log(err)
+    };
+  };
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setLocation((e.target.value));
+    
+  };
+
   return (
     <HomeWrapper>
-      <HomeHeadline>{t('attributes.titles.headline')}</HomeHeadline>
+      {/* <HomeHeadline>{t('attributes.titles.headline')}</HomeHeadline> */}
+      {/* Hello world ^ */}
+
+      <h1>Search for a restaurant by location!</h1>
+
+      <form onSubmit={onSubmit}>
+        <input type="text" id="search" name="location" value={location} onChange={onChange} />
+        <br />
+        <button type="submit">Search</button>
+      </form>
+
+
       <Outlet />
       <Helmet>
         <title lang={language}>{`${t('attributes.titles.headline')} - ${t(
