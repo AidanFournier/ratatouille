29345624f4c3e499d18611d3d7006e4f6fe1e73a
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-11-13 16:06:49 +0900

    Second api connected

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,7 +1,7 @@
 import { Helmet } from 'react-helmet';
 import { useTranslation } from 'react-i18next';
 import { Outlet } from 'react-router-dom';
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import axios from 'axios';
 
 import { Form } from './Form';
@@ -11,18 +11,13 @@ const defaultCoords = {
   lat: "",
   long: ""
 };
-// type Coords = {
-//   lat: string,
-//   long: string
-// };
 
 export function Home(): JSX.Element {
   const [t, { language }] = useTranslation();
 
-  const [ location, setLocation ] = useState("");
+  var location;
   const [ coords, setCoords ] = useState(defaultCoords);
-  const { lat, long } = coords;
-
+  const [ restaurants, setRestaurants ] = useState({});
 
   const urlCoords = `https://staging-snap.tablecheck.com/v2/autocomplete?locale=en&shop_universe_id=57e0b91744aea12988000001&text=${location}`;
   const urlShops = `https://staging-snap.tablecheck.com/v2/shop_search?cuisines[]=kaiseki&geo_latitude=${coords.lat}&geo_longitude=${coords.long}&shop_universe_id=57e0b91744aea12988000001&locale=en&per_page=50`
@@ -30,26 +25,27 @@ export function Home(): JSX.Element {
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     try { 
-      console.log(urlCoords)
       const res = await axios.get(urlCoords);
-      console.log(res.data);
-      const resLat: string = res.data.locations[0].payload.geo.lat.toString();
-      const resLong: string = res.data.locations[0].payload.geo.lon.toString();
       const newCoords = { 
-        lat: resLat,
-        long: resLong
-      }
+        lat: res.data.locations[0].payload.geo.lat.toString(),
+        long: res.data.locations[0].payload.geo.lon.toString()
+      };
       setCoords(newCoords)
-      // console.log(coords);
+
+      const resShops = await axios.get(urlShops);
+      setRestaurants(resShops.data.shops);
+
     } catch (err) {
       console.log('👹 ERROR:' + err)
     };
   };
 
   console.log(coords);
+  console.log(restaurants);
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setLocation((e.target.value));
+    // setLocation((e.target.value));
+    return location = e.target.value;
   };
 
   return (
