c68d5236d4e5a24e7a398b201c3e204c1c0941bb
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-11-14 22:56:31 +0900

    Added top nav and footer components

--- src/Pages/Restaurants/index.tsx
+++ src/Pages/Restaurants/index.tsx
@@ -4,17 +4,43 @@ import { useLocation } from 'react-router-dom';
 // import { useRestaurants } from '.../contexts/RestaurantContext'
 
 // import { HomeHeadline, HomeWrapper } from './styles';
+import { PageWrapper, PageContent, Headline, PageImage} from 'Layouts';
+import { PageLayout } from 'Layouts/Page';
+import { TopNav } from 'Layouts/TopNav'
+import { Footer } from 'Layouts/Footer'
 
 
+export function Restaurants({
+  isDarkMode,
+  setDarkMode
+}: {
+  isDarkMode: boolean;
+  setDarkMode: (value: boolean) => void;
+}
 
-export function Restaurants(): JSX.Element {
+): JSX.Element {
     const [t, { language }] = useTranslation();
     console.log("Hello from Restaurants");
 
     const location = useLocation();
     console.log(location.state);
   
-    return (
-    <div>Restaurants</div>
+  return (
+    <>
+      <TopNav isDarkMode={isDarkMode} setDarkMode={setDarkMode} />
+      <PageWrapper>
+        <PageContent>
+          <div>Restaurants</div>
+        </PageContent>
+
+
+        <Helmet>
+          <title lang={language}>{`${t('attributes.titles.headline')} - ${t(
+            'keywords.app_name'
+          )}`}</title>
+        </Helmet>
+      </PageWrapper>
+      <Footer />
+    </>
   )
 }
